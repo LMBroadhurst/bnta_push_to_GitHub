a24fde9bde314971e908dbfebe254ec1a8b2cee0
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-23 17:31:59 +0100

    Playing with the unique nft code

--- week_02/day_04/tdd/artGallery/artGallery/src/main/java/Artwork.java
+++ week_02/day_04/tdd/artGallery/artGallery/src/main/java/Artwork.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
+
 public class Artwork {
 
     private String title;
     private String artist;
     private int price;
     private String physical_nft;
+    private ArrayList<Artwork> all_artwork;
+    private int nft_code;
+
 
     public Artwork(String title, String artist, int price, String physical_nft) {
         this.title = title;
         this.artist = artist;
         this.price = price;
+        this.nft_code = uniqueNFT();
         this.physical_nft = physical_nft;
+
     }
 
 //    Getters and Setters
@@ -46,13 +53,32 @@ public class Artwork {
         this.physical_nft = physical_nft;
     }
 
+    public ArrayList<Artwork> getAll_artwork() {
+        return this.all_artwork;
+    }
+
+    public void setAll_artwork(ArrayList<Artwork> all_artwork) {
+        this.all_artwork = all_artwork;
+    }
+
+    public int getNft_code() {
+        return this.nft_code;
+    }
+
+    public void setNft_code(int nft_code) {
+        this.nft_code = nft_code;
+    }
 
 //    Behaviours
 
-    public void checkNftIsUnique() {
-
+    public int uniqueNFT() {
+      this.all_artwork.add(this);
+        return this.nft_code = all_artwork.size();
     }
 
+    public void addToArtUniverse(Artwork artwork) {
+        this.all_artwork.add(artwork);
+    }
 
 
 }
--- week_02/day_04/tdd/artGallery/artGallery/src/main/java/Main.java
+++ week_02/day_04/tdd/artGallery/artGallery/src/main/java/Main.java
@@ -1,127 +1,133 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-//        Customers
+////        Customers
+//
+//        Customer lewis = new Customer("Lewis", 100000);
+//        Customer falak = new Customer("Falak", 2000);
+//
+////        Testing customer behaviours
+//
+//        lewis.checkWalletBalance();
+//        falak.checkWalletBalance();
+//
+//        lewis.withdrawFromWallet(300);
+//        falak.depositToWallet(300);
+//
+//        lewis.checkWalletBalance();
+//        falak.checkWalletBalance();
+//
+//
+////        Galleries
+//
+//        Gallery saatchi = new Gallery("Saatchi Gallery", 10000);
+//        Gallery tateModern = new Gallery("Tate Modern", 8000);
+//
+////        Testing gallery behaviours
+//
+//        saatchi.checkCashReserve();
+//        tateModern.checkCashReserve();
+//
+//        tateModern.depositToCashReserve(5000);
+//        tateModern.checkCashReserve();
+//
+//
+////        Artworks
+//
+//        Artwork napalm = new Artwork("Napalm", "Banksy", 5000, "Physical");
+//        Artwork monaLisa = new Artwork("Mona Lisa", "Leonardo da Vinci", 8000, "nft");
+//
+////        Adding artworks to gallery
+//
+//        saatchi.addNewArtwork(monaLisa);
+//        System.out.println(saatchi.getArtworks().get(0).getTitle());
+//
+//        for (int i = 0; i < saatchi.getArtworks().size(); i++) {
+//            System.out.println(saatchi.getArtworks().get(i).getTitle());
+//        }
+//
+//        tateModern.addNewArtwork(napalm);
+//
+//
+////        Artworks to add to artist
+//
+//        Artist vincentVanGogh = new Artist("Vicent can Gogh", "VVG Portfolio", 100);
+//
+//        Artwork sunflowers1 = new Artwork("Sunflowers 1", "Vincent van Gogh", 10000, "Physical");
+//
+//        vincentVanGogh.AddArtToArtist(sunflowers1);
+//
+//        System.out.println(vincentVanGogh.getPortfolio().get(0).getTitle());
+//
+//        System.out.println(vincentVanGogh.getPortfolio().get(0).getPrice());
+//
+//        System.out.println("-------------");
+//
+//
+////        Sell artwork to gallery
+//
+//        System.out.println(saatchi.getCashReserves());
+//
+//        System.out.println(vincentVanGogh.getWallet());
+//
+//        vincentVanGogh.sellToGallery(sunflowers1, saatchi);
+//
+//        System.out.println(vincentVanGogh.getWallet());
+//
+//        System.out.println(saatchi.getCashReserves());
+//
+//        System.out.println("---------");
+//
+//
+//        //    Buy artwork from gallery (customer buys)
+//
+//        lewis.checkWalletBalance();
+//        lewis.printOutCollection();
+//        saatchi.checkCashReserve();
+//        saatchi.printOutCollection();
+//
+//        System.out.println("----");
+//
+//        lewis.buyArtFromGallery(saatchi, sunflowers1);
+//        lewis.buyArtFromGallery(saatchi, monaLisa);
+//
+//        System.out.println("----");
+//
+//        lewis.checkWalletBalance();
+//        lewis.printOutCollection();
+//        saatchi.checkCashReserve();
+//        saatchi.printOutCollection();
+//
+//        System.out.println("----");
+//
+////        Ability to calculate the value of its collection.
+//
+//        Artwork picture1 = new Artwork("Picture 1", "Lewis", 5000, "Yes");
+//        Artwork picture2 = new Artwork("Picture 2", "Lewis", 5000, "Yes");
+//        Artwork picture3 = new Artwork("Picture 3", "Lewis", 5000, "Yes");
+//        Artwork picture4 = new Artwork("Picture 4", "Lewis", 5000, "Yes");
+//
+//        saatchi.addNewArtwork(picture1);
+//        saatchi.addNewArtwork(picture2);
+//        saatchi.addNewArtwork(picture3);
+//        saatchi.addNewArtwork(picture4);
+//
+//        saatchi.stockTake();
+//    }
 
-        Customer lewis = new Customer("Lewis", 100000);
-        Customer falak = new Customer("Falak", 2000);
+        Artwork checkNFT1 = new Artwork("a", "a", 1, "a");
+        Artwork checkNFT2 = new Artwork("a", "a", 1, "a");
+        Artwork checkNFT3 = new Artwork("a", "a", 1, "a");
 
-//        Testing customer behaviours
+        checkNFT1.addToArtUniverse(checkNFT1);
+        checkNFT2.addToArtUniverse(checkNFT2);
+        checkNFT3.addToArtUniverse(checkNFT3);
 
-        lewis.checkWalletBalance();
-        falak.checkWalletBalance();
-
-        lewis.withdrawFromWallet(300);
-        falak.depositToWallet(300);
-
-        lewis.checkWalletBalance();
-        falak.checkWalletBalance();
-
-
-//        Galleries
-
-        Gallery saatchi = new Gallery("Saatchi Gallery", 10000);
-        Gallery tateModern = new Gallery("Tate Modern", 8000);
-
-//        Testing gallery behaviours
-
-        saatchi.checkCashReserve();
-        tateModern.checkCashReserve();
-
-        tateModern.depositToCashReserve(5000);
-        tateModern.checkCashReserve();
-
-
-//        Artworks
-
-        Artwork napalm = new Artwork("Napalm", "Banksy", 5000, "Physical");
-        Artwork monaLisa = new Artwork("Mona Lisa", "Leonardo da Vinci", 8000, "nft");
-
-//        Adding artworks to gallery
-
-        saatchi.addNewArtwork(monaLisa);
-        System.out.println(saatchi.getArtworks().get(0).getTitle());
-
-        for (int i = 0; i < saatchi.getArtworks().size(); i++) {
-            System.out.println(saatchi.getArtworks().get(i).getTitle());
-        }
-
-        tateModern.addNewArtwork(napalm);
-
-
-//        Artworks to add to artist
-
-        Artist vincentVanGogh = new Artist("Vicent can Gogh", "VVG Portfolio", 100);
-
-        Artwork sunflowers1 = new Artwork("Sunflowers 1", "Vincent van Gogh", 10000, "Physical");
-
-        vincentVanGogh.AddArtToArtist(sunflowers1);
-
-        System.out.println(vincentVanGogh.getPortfolio().get(0).getTitle());
-
-        System.out.println(vincentVanGogh.getPortfolio().get(0).getPrice());
-
-        System.out.println("-------------");
-
-
-//        Sell artwork to gallery
-
-        System.out.println(saatchi.getCashReserves());
-
-        System.out.println(vincentVanGogh.getWallet());
-
-        vincentVanGogh.sellToGallery(sunflowers1, saatchi);
-
-        System.out.println(vincentVanGogh.getWallet());
-
-        System.out.println(saatchi.getCashReserves());
-
-        System.out.println("---------");
-
-
-        //    Buy artwork from gallery (customer buys)
-
-        lewis.checkWalletBalance();
-        lewis.printOutCollection();
-        saatchi.checkCashReserve();
-        saatchi.printOutCollection();
-
-        System.out.println("----");
-
-        lewis.buyArtFromGallery(saatchi, sunflowers1);
-        lewis.buyArtFromGallery(saatchi, monaLisa);
-
-        System.out.println("----");
-
-        lewis.checkWalletBalance();
-        lewis.printOutCollection();
-        saatchi.checkCashReserve();
-        saatchi.printOutCollection();
-
-        System.out.println("----");
-
-//        Ability to calculate the value of its collection.
-
-        Artwork picture1 = new Artwork("Picture 1", "Lewis", 5000, "Yes");
-        Artwork picture2 = new Artwork("Picture 2", "Lewis", 5000, "Yes");
-        Artwork picture3 = new Artwork("Picture 3", "Lewis", 5000, "Yes");
-        Artwork picture4 = new Artwork("Picture 4", "Lewis", 5000, "Yes");
-
-        saatchi.addNewArtwork(picture1);
-        saatchi.addNewArtwork(picture2);
-        saatchi.addNewArtwork(picture3);
-        saatchi.addNewArtwork(picture4);
-
-        saatchi.stockTake();
+        System.out.println(checkNFT1.getNft_code());
+        System.out.println(checkNFT2.getNft_code());
+        System.out.println(checkNFT3.getNft_code());
     }
-
-
-
-
-
-
-
-
-
 }
