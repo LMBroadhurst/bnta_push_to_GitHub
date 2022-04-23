f2d3be61bead21da7dd1d8cad95f26837120a727
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-23 16:20:57 +0100

    More tests

--- week_02/day_04/tdd/artGallery/artGallery/src/test/java/GalleryTest.java
+++ week_02/day_04/tdd/artGallery/artGallery/src/test/java/GalleryTest.java
@@ -6,22 +6,64 @@ class GalleryTest {
 
     @Test
     void depositToCashReserve() {
+//        Given
+        Gallery testG = new Gallery("testG", 100);
+
+//        When
+        testG.depositToCashReserve(100);
+
+//        Then
+        assertEquals(200, testG.getCashReserves());
     }
 
     @Test
     void withdrawToBank() {
+//        Given
+        Gallery gTest = new Gallery("gTest", 100);
+
+//        When
+        gTest.withdrawToBank(100);
+
+//        Then
+        assertEquals(0, gTest.getCashReserves());
     }
 
     @Test
-    void checkCashReserve() {
+    void checkCashReserveTest() {
+//        Given
+        Gallery testGallery = new Gallery("test", 1000);
+
+//        When
+
+//        Then
+        assertEquals(1000, testGallery.getCashReserves());
     }
 
     @Test
-    void addNewArtwork() {
+    void addNewArtworkTest() {
+//        Given
+        Gallery testG = new Gallery("testG", 1000);
+        Artwork testA = new Artwork("testA", "a", 1, "a");
+
+//        When
+        testG.addNewArtwork(testA);
+
+//        Then
+        assertEquals("testA", testG.getArtworks().get(0).getTitle());
     }
 
     @Test
     void sellArtwork() {
+//        Given
+        Gallery tGal = new Gallery("tGal", 100);
+        Artwork tArt = new Artwork("tArt", "a", 1, "a");
+        tGal.addNewArtwork(tArt);
+
+//        When
+        tGal.sellArtwork(tArt);
+
+//        Then
+        assertEquals(true, tGal.getArtworks().isEmpty());
     }
 
     @Test
