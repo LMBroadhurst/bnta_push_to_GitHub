2ed892205cad8e5462de1cd725ec28ae2952a867
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-24 16:30:11 +0100

    Starting BNTA textbook notes

---
+++ notes_on_textbook/notes_on_java/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        A method is a function that belongs to a class. It is a "Behaviour" of that class.
+
+        for(String str : args) {
+//            str here is the element of the array that we are looping through.
+
+            System.out.println(str);
+        }
+    }
+
+}
