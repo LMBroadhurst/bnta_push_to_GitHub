460a6a5b51853e1f3f1cc269aa50aefb349a9051
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-27 09:39:01 +0100

    Start of day 03

---
+++ week_03/day_03/enums/src/main/java/org/example/Season.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public enum Season {
+    SPRING, SUMMER, FALL, WINTER
+}
+
+class SeasonWithoutEnum {
+    public static final int SPRING = 0;
+    public static final int SUMMER = 0;
+    public static final int FALL = 0;
+    public static final int WINTER = 0;
+
+}
