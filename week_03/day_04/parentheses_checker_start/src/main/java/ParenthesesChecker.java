4b5bb3a6203ec79d468537de940995a709d8a6e2
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-28 16:25:12 +0100

    All tests but 1

--- week_03/day_04/parentheses_checker_start/src/main/java/ParenthesesChecker.java
+++ week_03/day_04/parentheses_checker_start/src/main/java/ParenthesesChecker.java
@@ -5,11 +5,15 @@ public class ParenthesesChecker {
 
     boolean checkParentheses(String testString) {
 
+        boolean allBracketsMatched = false;
         boolean bracketsMatched = false;
+        boolean curlyBracketsMatched = false;
+        boolean squareBracketsMatched = false;
+        boolean arrowBracketsMatched = false;
 
         String[] splitTestString = testString.split("");
 
-        System.out.println(Arrays.toString(splitTestString));
+//        Testing brackets
 
         ArrayList<String> addBracketsHere = new ArrayList<>();
 
@@ -23,9 +27,56 @@ public class ParenthesesChecker {
             bracketsMatched = true;
         }
 
-        System.out.println(addBracketsHere);
-        System.out.println(addBracketsHere.size());
 
-        return bracketsMatched;
+        //        Testing square brackets
+
+        ArrayList<String> addCurlyBracketsHere = new ArrayList<>();
+
+        for (String character: splitTestString) {
+            if (character.equals("[") || character.equals("]")) {
+                addCurlyBracketsHere.add(character);
+            }
+        }
+
+        if (addCurlyBracketsHere.size() % 2 == 0) {
+            curlyBracketsMatched = true;
+        }
+
+
+        //        Testing  curly brackets
+
+        ArrayList<String> addSquareBracketsHere = new ArrayList<>();
+
+        for (String character: splitTestString) {
+            if (character.equals("{") || character.equals("}")) {
+                addSquareBracketsHere.add(character);
+            }
+        }
+
+        if (addSquareBracketsHere.size() % 2 == 0) {
+            squareBracketsMatched = true;
+        }
+
+        //        Testing arrow brackets
+
+        ArrayList<String> addArrowBracketsHere = new ArrayList<>();
+
+        for (String character: splitTestString) {
+            if (character.equals("<") || character.equals(">")) {
+                addArrowBracketsHere.add(character);
+            }
+        }
+
+        if (addArrowBracketsHere.size() % 2 == 0) {
+            arrowBracketsMatched = true;
+        }
+
+//        Combining statements
+
+        if (squareBracketsMatched && curlyBracketsMatched && bracketsMatched && arrowBracketsMatched) {
+            allBracketsMatched = true;
+        }
+
+        return allBracketsMatched;
     }
 }
