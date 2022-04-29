c22d3bcc1f3336925761e4822f5ae3b050d98187
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-04-29 13:56:40 +0100

    Finish morning recap

--- week_03/day_04/week3_recap/src/main/java/dataStructures/DSMain.java
+++ week_03/day_04/week3_recap/src/main/java/dataStructures/DSMain.java
@@ -1,4 +1,20 @@
 package dataStructures;
 
 public class DSMain {
+
+    public static void main(String[] args) {
+        Task task1 = new Task("Buy Milk");
+        Task task2 = new Task("Empty Bins");
+        Task task3 = new Task("Cut Grass");
+        Task task4 = new Task("Paint fence");
+
+        Diary diary = new Diary();
+
+        diary.addTask(task1);
+        diary.addTask(task2);
+        diary.addTask(task3);
+        diary.addTask(task4);
+
+    }
+
 }
