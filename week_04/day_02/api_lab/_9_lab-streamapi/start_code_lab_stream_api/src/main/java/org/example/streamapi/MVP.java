a543325f1693de9954d5d0d6b65d3a5c9167271d
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-03 17:24:11 +0100

    Stream API labs

---
+++ week_04/day_02/api_lab/_9_lab-streamapi/start_code_lab_stream_api/src/main/java/org/example/streamapi/MVP.java
@@ -0,0 +1,80 @@
+package org.example.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MVP {
+
+//    Convert a List<String> to an object stream and for each element -> System,out.println.
+
+    public void printNames(List<String> names) {
+        names.forEach(System.out::println);
+    }
+
+//        Given a List<Integers>, return a List<Integer> with event numbers.
+
+    public List<Integer> returnEvenNumbers(List<Integer> numbers) {
+        List<Integer> evenNumbers = numbers
+                .stream()
+                .filter(number -> number % 2 == 0)
+                .toList();
+
+        return evenNumbers;
+    }
+
+
+
+
+
+
+
+
+
+    /*
+        Given an int [], double the value of each int and return an int [].
+    */
+    public int[] doubleInts(int[] numbers) {
+        List<Integer> doubledArray = numbers
+                .stream()
+                .filter
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+    /*
+        Given a String, return a List<String>, all caps.
+     */
+    public List<String> splitToAllCapsList(String input) {
+        // Implement me :)
+        return null;
+    }
+
+    /*
+    Given a list of animals:
+     - filter the ones that start with a given letter
+     - return sorted List<String>, all caps.
+    */
+    public List<String> filterByFirstLetterAndOrder(List<String> list, String letter) {
+        // Implement me :)
+        return null;
+    }
+
+    /*
+        Given a list of words, return elements that:
+         - are shorter than the given number and
+         - start with a given letter.
+    */
+    public List<String> filterWords(List<String> words, int maxLength, String firstLetter) {
+        // Implement me :)
+        return null;
+    }
+}
