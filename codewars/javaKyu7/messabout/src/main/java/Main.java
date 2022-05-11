247646251f73682e7b62f6c373a98240aaa476a8
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-11 20:11:20 +0100

    Added complete kata

--- codewars/javaKyu7/messabout/src/main/java/Main.java
+++ codewars/javaKyu7/messabout/src/main/java/Main.java
@@ -1,3 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String number = "3 1 6 5 7 3";
+
+        String[] splitNumbStrings = number.split(" ");
+        int[] splitNumbInts = new int [splitNumbStrings.length];
+
+        for (int i = 0; i < splitNumbStrings.length; i++) {
+            splitNumbInts[i] = Integer.parseInt(splitNumbStrings[i]);
+        }
+
+        int lowest = splitNumbInts[0];
+        int highest = splitNumbInts[0];
+
+
+        for (int i = 1; i < splitNumbInts.length; i++) {
+            if (splitNumbInts[i] > highest) {
+                highest = splitNumbInts[i];
+            } else if (splitNumbInts[i] < lowest) {
+                lowest = splitNumbInts[i];
+            }
+        }
+
+        String output = Integer.toString(highest) + " " + Integer.toString(lowest);
+
+        System.out.println(output);
+
+    }
+}
 
 
 
