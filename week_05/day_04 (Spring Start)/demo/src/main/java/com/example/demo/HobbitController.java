d52b398eb54c346797d2e59e7e4032f93f6347ab
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-12 11:21:08 +0100

    Spring

---
+++ week_05/day_04 (Spring Start)/demo/src/main/java/com/example/demo/HobbitController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HobbitController {
+
+//        Create, Read, Update, Delete = POST, GET, PUT, DELETE
+
+}
+
