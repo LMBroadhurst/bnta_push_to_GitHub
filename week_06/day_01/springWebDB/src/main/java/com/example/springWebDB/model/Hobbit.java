ed27aae0338308ba4d6f8cc52dbdd6dae142a37a
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-16 19:34:43 +0100

    Finished springboot app

--- week_06/day_01/springWebDB/src/main/java/com/example/springWebDB/model/Hobbit.java
+++ week_06/day_01/springWebDB/src/main/java/com/example/springWebDB/model/Hobbit.java
@@ -1,11 +1,14 @@
 package com.example.springWebDB.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Hobbit {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String lastName;
