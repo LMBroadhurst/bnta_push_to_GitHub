dcf9b3111ce1ec2237a58b131ef8dc3f0d89af3a
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-18 14:47:20 +0100

    Started pokemon manytomany API

---
+++ week_06/day_03/pokemon-many-to-many/src/main/java/com/example/pokemonmanytomany/PokemonManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonmanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonManyToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonManyToManyApplication.class, args);
+	}
+
+}
---
+++ week_06/day_03/pokemon-many-to-many/src/test/java/com/example/pokemonmanytomany/PokemonManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonmanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
