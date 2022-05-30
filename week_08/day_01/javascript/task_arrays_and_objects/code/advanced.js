ec71bdcb1fc0014aba0e2e8ac4d0655ba2d7b37b
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-30 16:07:21 +0100

    Fixed pushing?

---
+++ week_08/day_01/javascript/task_arrays_and_objects/code/advanced.js
@@ -0,0 +1,81 @@
+/**
+ * These tasks are more complicated! Using combinations of arrays and objects is very common, here you will need to combine what you know about both to complete the tasks.
+ */
+
+
+unitedKingdom = [
+    {
+        name: "Scotland",
+        capital: "Edinburgh",
+        population: 5454000,
+        touristAttractions: [
+            "Stirling Castle",
+            "Loch Ness"
+        ]
+    },
+    {
+        name: "England",
+        capital: "London",
+        population: 55980000
+    },
+    {
+        name: "Wales",
+        capital: "Swansea",
+        population: 3163000,
+        touristAttractions: [
+            "Snowdonia"
+        ]
+    },
+    {
+        name: "Northern Ireland",
+        capital: "Belfast",
+        population: 1885000,
+        touristAttractions: [
+            "Titanic Museum",
+            "Giant's Causeway"
+        ]
+    }
+]
+
+
+/**
+ * Q1. Add some tourist attractions to England
+ */
+
+unitedKingdom[1].touristAttractions = ["Stonehenge", "Big Ben", "Bristol Docks"];
+console.log(england);
+
+
+
+/**
+ * Q2. Change the capital of Wales to "Cardiff"
+ */
+
+unitedKingdom[2].capital = "Cardiff";
+console.log(wales);
+
+
+
+/**
+ * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
+ */
+
+unitedKingdom[3];
+console.log(northernIrelandKeys);
+
+Object.keys(unitedKingdom[3])
+
+
+
+/**
+ * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
+ */
+
+otherCountryPopulation = unitedKingdom[2].population;
+scottishPopulation = unitedKingdom[0].population;
+
+if (otherCountryPopulation > scottishPopulation) {
+    console.log("Scottish population is smaller");
+} else {
+    console.log("Scottish population is bigger")
+}
