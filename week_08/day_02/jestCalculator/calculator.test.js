6caad2e0a2686f6c3aaba4eef58994e6a4c8baea
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-31 12:38:09 +0100

    Test coverage added

--- week_08/day_02/jestCalculator/calculator.test.js
+++ week_08/day_02/jestCalculator/calculator.test.js
@@ -1,14 +1,17 @@
 const {sum} = require('./calculator');
 
-test('Can add two small positive numbers', () => {
-    // Arrange - do any setup
+describe('addition functionality', () => {
 
-    // Act - call the function under test
-    const actual = sum(2, 3);
+    test('Can add two small positive numbers', () => {
+        // Arrange - do any setup
 
-    // Assert - call the assertion method
-    const expected = 5;
+        // Act - call the function under test
+        const actual = sum(2, 3);
 
-    expect(actual).toBe(expected);
+        // Assert - call the assertion method
+        const expected = 5;
+
+        expect(actual).toBe(expected);
+    });
 
 });
