98cbc95fdcfcea298bff78e835645d74ea2fa28f
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-31 09:57:33 +0100

    More scope

--- week_08/scope.js
+++ week_08/scope.js
@@ -1,5 +1,24 @@
-numbers = [1, 2, 3, 4, 5];
+let numbers = [1, 2, 3, 4, 5];
 
-for (number of numbers) {
-    console.log(`Value of number inside loop is ${number}.`)
+const numbersSquared = [];
+
+const numberToPrint = 0;
+let undefinedvariable;
+// Can assign a variable without declaring it... not for const though.
+
+for (let number of numbers) {
+    let numberToPrint = number;
+    console.log(`Value of numberToPrint inside the loop is ${numberToPrint}.`);
 }
+
+for (let number of numbers) {
+    numbersSquared.push(number ** 2);
+}
+
+console.log(numbersSquared);
+
+// let removes the global scope, and line 13 throws an error.
+// By using let, we have given the number value "BLOCK" access.
+
+console.log(`Value of numberToPrint outside the loop: ${numberToPrint}`);
+
