b0c9ec366019d7735606f68e01261714554d62bd
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-31 11:07:09 +0100

    finished functions

--- week_08/day_02/functions.js
+++ week_08/day_02/functions.js
@@ -33,4 +33,28 @@ function greet3(timeOfDay, name) {
 // 
 // Anonymous Functions
 // 
+// Hoisting does not work here though...
+
+const sum = function (number1, number2) {
+    return number1 + number2;
+}
+
+const total = sum(1, 2);
+
+console.log(total);
+
+
+
+// 
+// Arrow Function // 
+// Big boiz
+// Still anonymous...
+
+const multiply = (number1, number2) => {
+    return number1 * number2;
+}
+
+const product = multiply(4, 5);
+
+console.log(product);
 
