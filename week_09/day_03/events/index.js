82c957ac0a761eb8e76b3284e35ad187bbd4a6fe
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-08 12:39:57 +0100

    More Dom Manipulation

--- week_09/day_03/events/index.js
+++ week_09/day_03/events/index.js
@@ -13,3 +13,14 @@ const message = document.querySelector("#user-message")
 input.addEventListener("input", (event) => {
     message.innerText = event.target.value;
 })
+
+const list = document.querySelector("ul");
+const form = document.querySelector("#new-student-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newListItem = document.createElement("li");
+    newListItem.innerText = event.target["new-student"].value;
+    console.log(event);
+    list.appendChild(newListItem);
+});
