6bf5ed78300767ef09d9b9b7949ad79abb4d9c25
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-08 12:10:12 +0100

    added event listener

--- week_09/day_03/events/index.js
+++ week_09/day_03/events/index.js
@@ -1,7 +1,15 @@
 const button = document.querySelector("#btn");
+const box = document.querySelector("#box");
 
 const handleButtonClick = () => {
-    console.log("button clicked");
+    box.style.backgroundColor = "blue";
 }
 
 button.addEventListener('click', handleButtonClick);
+
+const input = document.querySelector("#user-input");
+const message = document.querySelector("#user-message")
+
+input.addEventListener("input", (event) => {
+    message.innerText = event.target.value;
+})
